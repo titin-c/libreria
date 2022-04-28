424e8e3ea956107b91164a8cc40fe5abb06235bf
Your NameTitín <titin.colomer.romero@gmail.com>
2022-04-28 13:44:05 +0200

    primer commit

--- src/components/inicio/Inicio.js
+++ src/components/inicio/Inicio.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 export const Inicio = () => {
   return (
-    <div  className="cont">
-    <h1>Libreria de recursos</h1>
+    <div className="cont">
+      <h1>Libreria de recursos</h1>
 
       <h2>Elementos de front-end realizados con React & Sass</h2>
-      <p>No se trata de una librería comercial al uso, es más una cocina donde experimento y un almacen de recursos que suelo 
-        reutilizar constantemente... Al ser de uso personal, no he sentido la necesidad de optimizar esta página, para los distintos 
-        dispositivos. Disculpen las molestias... quizás en un futuro.<br/>
+      <p>No se trata de una librería comercial al uso, es más una cocina donde experimento y un almacen de recursos que suelo
+        reutilizar constantemente... Al ser de uso personal, no he sentido la necesidad de optimizar esta página, para los distintos
+        dispositivos. Disculpen las molestias... quizás en un futuro.<br />
         Para realizar, cada uno de los elementos se ha utilizado únicamente react, y scss</p>
 
       <p><i>By Titín Colomer</i></p>
-  </div>
+    </div>
   )
 }
